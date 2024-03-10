5bd29863bcf75b3dced32b6e9bf5f27537bbda11
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2024-03-11 01:17:34 +0500

    fix dot

--- components/macro/title.js
+++ components/macro/title.js
@@ -4,7 +4,7 @@ function Title({className}) {
   return (
     <div className={`font-bold px-2 text-[55px] md:text-[80px] xl:text-[112px] leading-normal text-white mx-auto text-center min-w-min ${className}`}>
       <div>Own your audience.</div>
-      <p className="leading-[110%] bg-gradient-to-r bg-clip-text text-transparent from-[rgb(254,103,131)] to-[rgb(255,191,132)] animate-gradient bg-repeat" style={{"backgroundSize": "300%"}}>So you don't lose them</p>
+      <p className="leading-[110%] bg-gradient-to-r bg-clip-text text-transparent from-[rgb(254,103,131)] to-[rgb(255,191,132)] animate-gradient bg-repeat" style={{"backgroundSize": "300%"}}>{`So you don't lose them`}</p>
     </div>
   );
 }
