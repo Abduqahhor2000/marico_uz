c7adcf725ac73a476741e0a7852d2a5756e93425
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2024-03-12 00:58:16 +0500

    add my tg link

--- components/micro/button.js
+++ components/micro/button.js
@@ -1,15 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
-function Button({ variant="contained", className="", children="", ...props }) {
+function Button({
+  variant = "contained",
+  className = "",
+  children = "",
+  ...props
+}) {
   return (
-    <div
-      className={`select-none text-center rounded-xl pt-[18px] pb-[17px] px-[22px] text-xl leading-normal font-medium border duration-200 hover:cursor-pointer active:opacity-80 ${
-        variant === "text" ? "border-transparent text-border hover:text-white" : variant === "outlined" ? " border-border text-border hover:text-white hover:border-white" : "bg-primary border-primary text-white "
-      } ${className}`}
-      {...props}
-    >
-      {children}
-    </div>
+    <Link href={"https://t.me/deyil"}>
+      <div
+        className={`select-none text-center rounded-xl pt-[18px] pb-[17px] px-[22px] text-xl leading-normal font-medium border duration-200 hover:cursor-pointer active:opacity-80 ${
+          variant === "text"
+            ? "border-transparent text-border hover:text-white"
+            : variant === "outlined"
+            ? " border-border text-border hover:text-white hover:border-white"
+            : "bg-primary border-primary text-white "
+        } ${className}`}
+        {...props}
+      >
+        {children}
+      </div>
+    </Link>
   );
 }
 
